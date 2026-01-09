e9f3cf4baf25f77a6a015b166274b440a9010977
Moamal Hussin <dev.moamal@gmail.com>
2026-01-09 19:39:26 +0300

    refactor: Adding docs to the file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/** Merge class names
+ * @param inputs - Class values to merge
+ * @returns Merged class names
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
